67d6e7d757c6ce9412ab996cae2095dc0f84eecd
Heather Barr <hnbarr@yahoo.com>
2020-08-29 17:11:30 -0500

    adding some sample text to app's intro component

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,10 +6,10 @@ export default class Intro extends Component {
             <div id="IntroComponent">
                 <div class="jumbotron">
                     <h1 class="display-4">My Voice Matters</h1>
-                    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                    <p class="lead">Here to help you understand our goverment's bits n pieces. Because if you're like us, I'm sure having some help with knowing who makes what moves in this place and how we can actually make a difference with our vote.</p>
                     <hr class="my-4"/>
-                    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                    <p>We're just two people trying to do our part in making an actual difference, the best that we can.</p>
+                    <a class="btn btn-light btn-lg" href="#" role="button">Learn more</a>
                 </div>
             </div>
         )
