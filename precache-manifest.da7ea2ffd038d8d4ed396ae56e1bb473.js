914bef815868358563fa0e4e83c4dd982aaa0d2e
Heather Barr <hnbarr@yahoo.com>
2020-11-17 13:15:17 -0600

    Updates

---
+++ precache-manifest.da7ea2ffd038d8d4ed396ae56e1bb473.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19b28d833069068352201fcb22bed938",
+    "url": "/hnbarr/voice/index.html"
+  },
+  {
+    "revision": "2150372e4afc6d9d5c1b",
+    "url": "/hnbarr/voice/static/css/main.d34dcfb9.chunk.css"
+  },
+  {
+    "revision": "6401bbea9891a336993a",
+    "url": "/hnbarr/voice/static/js/2.6f89fa02.chunk.js"
+  },
+  {
+    "revision": "a1f2d16d999162287306f097f879a2dd",
+    "url": "/hnbarr/voice/static/js/2.6f89fa02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2150372e4afc6d9d5c1b",
+    "url": "/hnbarr/voice/static/js/main.829ce637.chunk.js"
+  },
+  {
+    "revision": "09ebc9df03b547d8ab9b",
+    "url": "/hnbarr/voice/static/js/runtime-main.c7184158.js"
+  },
+  {
+    "revision": "2541cc282e0dfa8d416ab91b1e34b8fa",
+    "url": "/hnbarr/voice/static/media/cork-board.2541cc28.png"
+  },
+  {
+    "revision": "8939bf293e12aa837a2b92645724f327",
+    "url": "/hnbarr/voice/static/media/linedpaper.8939bf29.png"
+  },
+  {
+    "revision": "33545ec6ffda90e786dea627ce363688",
+    "url": "/hnbarr/voice/static/media/memphis-mini.33545ec6.png"
+  },
+  {
+    "revision": "044ec9889a76d5669d922680d31a4f19",
+    "url": "/hnbarr/voice/static/media/vintage-concrete.044ec988.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/voice/precache-manifest.b0dbdf160892b4d12949fd794f2a9a64.js"
+  "/hnbarr/voice/precache-manifest.da7ea2ffd038d8d4ed396ae56e1bb473.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/voice/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hnbarr/voice/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
